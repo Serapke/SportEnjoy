b62e70e212ad2b3e0eec45cc4c46220d3f7b835b
Mantas <M.Serapinas@gmail.com>
2016-09-14 21:14:26 +0300

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { SpotsComponent } from './spots/spots.component';
 import { SpottersComponent } from './spotters/spotters.component';
 // import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
-// import { ReviewComponent } from './review/review.component';
+import { ReviewComponent } from './review/review.component';
 import { SpotAddComponent } from './spots/spot-add/spot-add.component';
 // import { SpotUpdateComponent } from './spots/spot-update/spot-update.component';
 import { ProfileComponent } from './profile/profile.component';

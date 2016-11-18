0573cafa4fd8aa5192252eb1d194b0c1bcc05ac1
Mantas <M.Serapinas@gmail.com>
2016-11-18 13:57:05 +0000

    spots refactored

--- src/app/spots/spots.component.ts
+++ src/app/spots/spots.component.ts
@@ -1,226 +1,228 @@
-import { Component, OnInit, OnDestroy, HostListener, NgZone } from '@angular/core';
+import { Component, OnInit, HostListener, NgZone } from '@angular/core';
 import { ROUTER_DIRECTIVES, ActivatedRoute, Router } from '@angular/router';
 import 'rxjs/Rx';
 import { LocationService } from '../shared/location/location.service';
-import { SpottersJoinDirective } from '../shared/spotters-join/spotters-join.directive'; 
+import { SpottersJoinDirective } from '../shared/spotters-join/spotters-join.directive';
 import { SpotService } from './spot.service';
 import { Observable } from 'rxjs/Observable';
 import { ISpot } from './spot';
 import { SpotFilterPipe } from './spot-filter.pipe';
 import { SebmGoogleMapInfoWindow, SebmGoogleMap} from 'angular2-google-maps/core';
+import { TextTransformService } from '../shared/text-transform.service';
 
 @Component({
-	templateUrl: './spots.component.html',
-	styleUrls: ['./spots.component.css'],
-	pipes: [SpotFilterPipe],
-	directives: [
-		ROUTER_DIRECTIVES,
-		SebmGoogleMap,
-		SpottersJoinDirective
-	]
+  templateUrl: './spots.component.html',
+  styleUrls: ['./spots.component.css'],
+  pipes: [SpotFilterPipe],
+  directives: [
+    ROUTER_DIRECTIVES,
+    SebmGoogleMap,
+    SpottersJoinDirective
+  ]
 })
-export class SpotsComponent implements OnInit, OnDestroy{
-	private sub: any;
-	showSearchProperties: number = 0;
-	rotatedSearchPropertiesArrow: boolean = false;
-	allSpots: ISpot[];
-	particularSpots: ISpot[];
-	errorMessage: string;
-	selectedCity: string = '';
-	selectedCategory: string = '';
-	selectedSorting: string = '';
-	listFilter: string = '';
-	defaultImage: string = '/sportenjoy/assets/images/spotter-background.jpg';
-	lat: number = 54.8;
-	lng: number = 23.9;
-	zoom: number = 6;
-	mapDraggable: boolean;
-	showSortProperties: boolean = false;
-	selectedSpot: ISpot;
-	openInfoWindow: SebmGoogleMapInfoWindow;
-	
+export class SpotsComponent implements OnInit {
+  showingLocalSpots: boolean = false;
 
-	constructor(
-		private _spotService: SpotService,
-		private _route: ActivatedRoute,
-		private _locationService: LocationService,
-		private _ngZone: NgZone,
-	  	private _router: Router
-	) {	
-		this.mapDraggable = document.body.clientWidth < 992 ? false : true;
-	}
+  spots: ISpot[];
 
-	@HostListener('window:resize', ['$event'])
-	onResize(event) {
-		this.mapDraggable = event.target.innerWidth < 992 ? false : true;
-	}
+  showSearchProperties: number = 0;
+  rotatedSearchPropertiesArrow: boolean = false;
 
-	toggleSearchProperties(): void {
-		if (this.showSearchProperties == 0) {
-			this.showSearchProperties = 189;
-			this.rotatedSearchPropertiesArrow = !this.rotatedSearchPropertiesArrow;
-			return;
-		}
-		this.rotatedSearchPropertiesArrow = !this.rotatedSearchPropertiesArrow;
-		this.showSearchProperties = 0;
-	}
-	clickedMarker(spot: ISpot, i, infoWindow): void {
-		console.log(infoWindow);
-		this.selectedSpot = spot;
-		this.lat = this.selectedSpot.latitude;
-		this.lng = this.selectedSpot.longitude;
-		this.makeSelectedSpotFirst(i);
+  errorMessage: string;
+  selectedCity: string;
+  selectedCategory: string;
+  selectedPage: number;
+  selectedSorting: string = '';
+  listFilter: string = '';
+  defaultImage: string = '/sportenjoy/assets/images/spotter-background.jpg';
 
-		if (this.openInfoWindow && this.openInfoWindow !== infoWindow){
-			this.openInfoWindow.close();
-		}
-		this.openInfoWindow = infoWindow;
-	}
-	toggleDraggability(): void {
-		this.mapDraggable = !this.mapDraggable;
-	}
-	toggleSortProperties(): boolean {
-		return this.showSortProperties = !this.showSortProperties;
-	}
-	gotoDetail(spot: ISpot) {
-		this._router.navigate(['/spot', spot.id]);
-	}
-	getAllSpots(): void {
-		this._spotService.getSpots()
-         .subscribe(
-           spots => {
-				this.allSpots = spots;
-				this.particularSpots = spots;
-				this.sortSpotsByRating();
-				this.selectedSorting = 'rating';
-				this.getParams();
-			},
-			error =>  {
-				this.errorMessage = <any>error;
-				console.log(this.errorMessage);
-			}
-		);
-	}
-	getAllCategorySpots(): void {
-		this.selectedCategory = '';
-		this.particularSpots = this._spotService.getParticularCitySpots(this.allSpots, this.selectedCity);
-	}
-	getAllCitySpots(): void {
-		this.selectedCity = '';
-		this.particularSpots = this._spotService.getParticularCategorySpots(this.allSpots, this.selectedCategory);
-	}
+  // Map params
+  centerLat: number = 54.8;
+  centerLng: number = 23.9;
+  userLat: number;
+  userLng: number;
+  zoom: number = 6;
+  mapDraggable: boolean;
 
-	getLocalSpots(): void {
-		this._locationService.getUserLocation()
-			.subscribe(position => {
-				console.log("User position is:\nLatitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
-			}, error => {
-				console.error("Error while trying to get user location");
-			});
-	}
+  showSortProperties: boolean = false;
+  selectedSpot: ISpot;
+  openInfoWindow: SebmGoogleMapInfoWindow;
+  userLocation: any;
 
-	getParticularSpots(category: string, city: string) {
-		console.log("Get particular spots: "  + category + " " + city);
-		if (category.length != 0 && category !== this.selectedCategory) {
-			this.getAllCategorySpots();
-			this.selectedCategory = category;
-		}
-		if (city.length != 0 && city !== this.selectedCity) {
-			this.getAllCitySpots();
-			this.selectedCity = city;
-		}
-
-		this.particularSpots = this._spotService.getParticularCategorySpots(this.particularSpots, this.selectedCategory);
-		this.particularSpots = this._spotService.getParticularCitySpots(this.particularSpots, this.selectedCity);
-	}
-
-	ngOnInit(): void {
-		this.getAllSpots();
-		this.getLocalSpots();
-  	}
-
-	getParams():void {
-		this.sub = this._route
-			.params
-			.subscribe(params => {
-				this.selectedCity = params['location'];
-				this.selectedCategory = params['category'];
-				if (this.selectedCategory) {
-					this.selectedCategory = this.selectedCategory.replace(/%C5%A1/g, 'š');
-					this.selectedCategory = this.selectedCategory.replace(/g%20/g, ' ');
-				}
-				this.selectedCity = this.selectedCity ? this.selectedCity : '';
-				this.selectedCategory = this.selectedCategory ? this.selectedCategory : '';
-				if (this.selectedCity != '' && this.selectedCategory != '') {
-					this.getParticularSpots(this.selectedCategory, this.selectedCity);
-				}
-				else { this.particularSpots = this.allSpots }
-			});
-	}
-
-	ngOnDestroy() {
-    this.sub.unsubscribe();
+  constructor(
+    private _spotService: SpotService,
+    private _route: ActivatedRoute,
+    private _locationService: LocationService,
+    private _textTransformService: TextTransformService,
+    private _ngZone: NgZone,
+      private _router: Router
+  ) {
+    this.mapDraggable = document.body.clientWidth < 992 ? false : true;
   }
 
-	sortSpotsByAlphabet(): void {
-		this.selectedSorting = 'alphabet';
-		let spots = this.selectedSpot ? this.particularSpots.slice(1) : this.particularSpots;
-		let sortedSpotters = spots.sort((a, b) => {
-			if (this.selectedSpot && (a.id == this.selectedSpot.id || b.id == this.selectedSpot.id))
-				return 0;
-			if (a.title > b.title) {
-				return 1;
-			}
-			if (a.title < b.title) {
-				return -1;
-			}
-			return 0;
-		});
-		this.particularSpots = sortedSpotters;
-		if (this.selectedSpot) this.particularSpots.unshift(this.selectedSpot);
-	}
+  ngOnInit(): void {
+    this.getSpots();
+  }
 
-	sortSpotsByRating(): void {
-		this.selectedSorting = 'rating';
-		let spots = this.selectedSpot ? this.particularSpots.slice(1) : this.particularSpots;
-		let sortedSpotters = spots.sort((a, b) => {
-			if (this.selectedSpot && (a.id == this.selectedSpot.id || b.id == this.selectedSpot.id))
-				return 0;
-			if (a.rating < b.rating) {
-				return 1;
-			}
-			if (a.rating > b.rating) {
-				return -1;
-			}
-			return 0;
-		});
-		this.particularSpots = sortedSpotters;
-		if (this.selectedSpot) this.particularSpots.unshift(this.selectedSpot);
-	}
+  getSpots(): void {
+    this._route
+      .params
+      .subscribe(params => {
+        this.selectedCity = params['location'];
+        this.selectedCategory = params['category'];
+        this.selectedPage = parseInt(params['page']);
 
-	sortSpotsByBeenHere(): void {
-		this.selectedSorting = 'beenHere';
-		let spots = this.selectedSpot ? this.particularSpots.slice(1) : this.particularSpots;
-		let sortedSpotters = spots.sort((a, b) => {
-			if (a.beenHere < b.beenHere) {
-				return 1;
-			}
-			if (a.beenHere > b.beenHere) {
-				return -1;
-			}
-			return 0;
-		});
-		this.particularSpots = sortedSpotters;
-		if (this.selectedSpot) this.particularSpots.unshift(this.selectedSpot);
-	}
+        this.selectedCategory = this._textTransformService.escapeCharacters(this.selectedCategory);
 
-	makeSelectedSpotFirst(i) {
-		let first = this.particularSpots[0];
-		this.particularSpots[0] = this.selectedSpot;
-		this.particularSpots[i] = first;
-	}
+        if (this.selectedCity && this.selectedCategory && this.selectedPage) {
+          console.log(this.selectedCity + " " + this.selectedCategory + " " + this.selectedPage);
+          this.getParticularSpots(this.selectedCategory, this.selectedCity, this.selectedPage);
+        } else {
+          this.getAllSpots();
+          this.getLocalSpots();
+        }
+      });
+  }
 
-	gotoSpotters() {
-		this._router.navigate(['/spotters']);
-	}
+  getParticularSpots(category: string, city: string, page: number) {
+    console.log("Get particular spots: "  + category + " " + city);
+    this._spotService.getParticularSpots(city, category, page)
+      .subscribe(spots => {
+        this.spots = spots;
+      });
+  }
+
+  getAllSpots(): void {
+    this._spotService.getSpots()
+          .subscribe(spots => {
+        this.spots = spots;
+        this.sortSpotsByRating();
+        this.selectedSorting = 'rating';
+      }, error => {
+        this.errorMessage = <any>error;
+        console.log(this.errorMessage);
+      });
+  }
+
+  getLocalSpots(): void {
+    this._locationService.getUserLocation()
+      .subscribe(position => {
+        this.userLocation = position.coords;
+        this._spotService.getLocalSpots(this.userLocation)
+          .subscribe(spots => {
+            // Setting user location marker
+            this.userLat = this.userLocation.latitude;
+            this.userLng = this.userLocation.longitude;
+            // Centering map to user location
+            this.centerLat = this.userLocation.latitude;
+            this.centerLng = this.userLocation.longitude;
+
+            this.showingLocalSpots = true;
+            this.zoom = 12;
+            // -----------
+            this.spots = spots;
+          });
+      }, error => {
+        this.errorMessage = error;
+      });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.mapDraggable = event.target.innerWidth < 992 ? false : true;
+  }
+
+  toggleSearchProperties(): void {
+    if (this.showSearchProperties == 0) {
+      this.showSearchProperties = 189;
+      this.rotatedSearchPropertiesArrow = !this.rotatedSearchPropertiesArrow;
+      return;
+    }
+    this.rotatedSearchPropertiesArrow = !this.rotatedSearchPropertiesArrow;
+    this.showSearchProperties = 0;
+  }
+  clickedMarker(spot: ISpot, i, infoWindow): void {
+    console.log(infoWindow);
+    this.selectedSpot = spot;
+    // Center map on selected marker
+    this.centerLat = this.selectedSpot.latitude;
+    this.centerLng = this.selectedSpot.longitude;
+    this.makeSelectedSpotFirst(i);
+
+    if (this.openInfoWindow && this.openInfoWindow !== infoWindow){
+      this.openInfoWindow.close();
+    }
+    this.openInfoWindow = infoWindow;
+  }
+  toggleDraggability(): void {
+    this.mapDraggable = !this.mapDraggable;
+  }
+  toggleSortProperties(): boolean {
+    return this.showSortProperties = !this.showSortProperties;
+  }
+  gotoDetail(spot: ISpot) {
+    this._router.navigate(['/spot', spot.id]);
+  }
+
+  sortSpotsByAlphabet(): void {
+    this.selectedSorting = 'alphabet';
+    let spots = this.selectedSpot ? this.spots.slice(1) : this.spots;
+    let sortedSpotters = spots.sort((a, b) => {
+      if (this.selectedSpot && (a.id == this.selectedSpot.id || b.id == this.selectedSpot.id))
+        return 0;
+      if (a.title > b.title) {
+        return 1;
+      }
+      if (a.title < b.title) {
+        return -1;
+      }
+      return 0;
+    });
+    this.spots = sortedSpotters;
+    if (this.selectedSpot) this.spots.unshift(this.selectedSpot);
+  }
+
+  sortSpotsByRating(): void {
+    this.selectedSorting = 'rating';
+    let spots = this.selectedSpot ? this.spots.slice(1) : this.spots;
+    let sortedSpotters = spots.sort((a, b) => {
+      if (this.selectedSpot && (a.id == this.selectedSpot.id || b.id == this.selectedSpot.id))
+        return 0;
+      if (a.rating < b.rating) {
+        return 1;
+      }
+      if (a.rating > b.rating) {
+        return -1;
+      }
+      return 0;
+    });
+    this.spots = sortedSpotters;
+    if (this.selectedSpot) this.spots.unshift(this.selectedSpot);
+  }
+
+  sortSpotsByBeenHere(): void {
+    this.selectedSorting = 'beenHere';
+    let spots = this.selectedSpot ? this.spots.slice(1) : this.spots;
+    let sortedSpotters = spots.sort((a, b) => {
+      if (a.beenHere < b.beenHere) {
+        return 1;
+      }
+      if (a.beenHere > b.beenHere) {
+        return -1;
+      }
+      return 0;
+    });
+    this.spots = sortedSpotters;
+    if (this.selectedSpot) this.spots.unshift(this.selectedSpot);
+  }
+
+  makeSelectedSpotFirst(i) {
+    let first = this.spots[0];
+    this.spots[0] = this.selectedSpot;
+    this.spots[i] = first;
+  }
+
+  gotoSpotters() {
+    this._router.navigate(['/spotters']);
+  }
 }

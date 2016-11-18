bcf62dcb7bd8cc928fbeae0587eda8d3beebaf86
Mantas <M.Serapinas@gmail.com>
2016-11-18 13:36:54 +0000

    login refacotr

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -12,62 +12,58 @@ export class LoginService {
     redirectUrl: string;
 
     constructor(
-        private _http: Http
+      private _http: Http
     ){
-        this.loggedIn = !!localStorage.getItem('auth_token') && !!localStorage.getItem('user');
+      this.loggedIn = !!localStorage.getItem('auth_token') && !!localStorage.getItem('user');
     }
 
     login(email: string, password: string): Observable<IUser> {
-        console.info("Sending login requst");
-        console.info("email: " + email + "; password: " + password);
-        let headers = new Headers({'Content-Type': 'application/json'});
-        return this._http.post(this._loginUrl, JSON.stringify({email, password}), {headers: headers})
-            .map((response: Response) => <IUser> response.json())
-            .do(data => {
-                            console.log("Succcesfully logged in! User: " + data.email);
-                            console.log(data);
-                            localStorage.setItem('auth_token', data.auth_token);
-                            localStorage.setItem('user_name', data.name);
-                            localStorage.setItem('user', JSON.stringify(data));
-                            this.loggedIn = true;
-                        })
-            .catch(this.handleError);
+      let headers = new Headers({'Content-Type': 'application/json'});
+      return this._http.post(this._loginUrl, JSON.stringify({email, password}), {headers: headers})
+        .map((response: Response) => <IUser> response.json())
+        .do(data => {
+            console.log("Succcesfully logged in! User: " + data.email);
+            localStorage.setItem('auth_token', data.auth_token);
+            localStorage.setItem('user_name', data.name);
+            localStorage.setItem('user', JSON.stringify(data));
+            this.loggedIn = true;
+        })
+        .catch(this.handleError);
     }
 
     logout() {
-        let auth_token = {"id": localStorage.getItem('auth_token')};
-        let headers = new Headers({'Content-Type': 'application/json'});
-        this._http.delete(this._loginUrl + "/" + localStorage.getItem('auth_token'), {headers: headers})
-                .map((response: Response) => console.log(response));
-        localStorage.removeItem('auth_token');
-        localStorage.removeItem('user_name');
-        localStorage.removeItem('user');
-        this.loggedIn = false;
+      let auth_token = {"id": localStorage.getItem('auth_token')};
+      let headers = new Headers({'Content-Type': 'application/json'});
+      this._http.delete(this._loginUrl + "/" + localStorage.getItem('auth_token'), {headers: headers})
+        .map((response: Response) => console.log(response));
+      localStorage.removeItem('auth_token');
+      localStorage.removeItem('user_name');
+      localStorage.removeItem('user');
+      this.loggedIn = false;
     }
 
     isLoggedIn() {
-        return this.loggedIn;
+      return this.loggedIn;
     }
 
     getCurrentUser(): IUser {
-        return <IUser> JSON.parse(localStorage.getItem('user'));
+      return <IUser> JSON.parse(localStorage.getItem('user'));
     }
 
     isModerator() {
-        let user = <IUser> JSON.parse(localStorage.getItem('user'));
-        if (this.isLoggedIn() && user.moderator)
-            return true;
-        return false;
+      let user = <IUser> JSON.parse(localStorage.getItem('user'));
+      if (this.isLoggedIn() && user.moderator)
+        return true;
+      return false;
     }
     isAdmin() {
-        let user = <IUser> JSON.parse(localStorage.getItem('user'));
-        if (this.isLoggedIn() && user.admin)
-            return true;
-        return false;
+      let user = <IUser> JSON.parse(localStorage.getItem('user'));
+      if (this.isLoggedIn() && user.admin)
+        return true;
+      return false;
     }
 
-		private handleError(error: Response) {
-       return Observable.throw(error.json().errors || 'Server error');
+    private handleError(error: Response) {
+      return Observable.throw(error.json().errors || 'Server error');
     }
-
 }

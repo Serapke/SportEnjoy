28db75b5fa59af6368f707744ab8692f014f6ab6
Mantas <M.Serapinas@gmail.com>
2016-12-24 16:57:56 +0200

    Facebook login integration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
 
+declare const FB:any;
+
 @Component({
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor() {
+    FB.init({
+      appId      : '349303705453014',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+  }
 
+  onFacebookLoginClick() {
+    FB.login();
+  }
+
+  statusChangeCallback(resp) {
+    if (resp.status === 'connected') {
+      // connect here with your server for facebook login by passing access token given by facebook
+      console.log("Successfuly logged in with Facebook");
+    }else if (resp.status === 'not_authorized') {
+
+    }else {
+
+    }
+  };
+  ngOnInit() {
+    FB.getLoginStatus(response => {
+      this.statusChangeCallback(response);
+    });
+  }
 }
